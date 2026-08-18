ab5220ebda304b96a482e1e6e2f4ccdb7e90796f
Nicolas Silva <sxnsilva@gmail.com>
2026-08-17 21:26:43 -0300

    menu responsivo adicionado na página inicial

---
+++ src/script/script.js
@@ -0,0 +1,6 @@
+const buttonContainer = document.getElementsByClassName('burguer-btn-content')[0];
+const nav = document.getElementsByTagName('nav')[0];
+
+buttonContainer.addEventListener('click', () => {
+    nav.classList.toggle('menu-active')
+}) 
