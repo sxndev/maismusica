57c0630e5714ed84925edca93f70d4bd780e727e
Nicolas Silva <sxnsilva@gmail.com>
2026-08-18 22:17:56 -0300

    animação do menu hambuguer no mobile corrigida

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,4 @@
 const buttonContainer = document.getElementsByClassName('burguer-btn-content')[0];
 const nav = document.getElementsByTagName('nav')[0];
 
-buttonContainer.addEventListener('click', () => {
-    nav.classList.toggle('menu-active')
-}) 
+buttonContainer.addEventListener('click', () => { nav.classList.toggle('menu-active') }); 
